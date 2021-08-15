b99204b40ab1ee5169afddb7c0a3810a4b907649
alwinpamintuan <jrpamintuan1@up.edu.ph>
2021-08-15 22:04:42 +0800

    Revert mode to headful, await for cat-img to load

--- index.mjs
+++ index.mjs
@@ -2,14 +2,17 @@ import puppeteer from 'puppeteer'
 
 (async () => {
     const browser = await puppeteer.launch({
-        headless: true,
+        headless: false,
         defaultViewport: null,
         userDataDir: './data_cache'
     })
     
     const page = await browser.newPage()
-    await page.goto('https://popcat.click/')
-    await page.waitForSelector('div.cat-img')
+    
+    await Promise.all([
+        await page.goto('https://popcat.click/'),
+        await page.waitForSelector('div.cat-img')
+    ])
 
     const cat = await page.$('div.cat-img')
     await page.hover('div.cat-img')
